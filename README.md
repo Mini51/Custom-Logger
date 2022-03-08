6374e4f5f4a3d530efcfbcdf7a54e3c11a726be2
Mini <80361877+Mini51@users.noreply.github.com>
2022-03-07 16:52:41 -0800

    main commit

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# Custom Logger
- A custom logger module 
+<h1 align='center'>Custom-logger</h1>
+<p align="center">A custom NodeJS logging system</p>
+
+<br>
+
+<p style="font-weight:bold">Functions</p>
+
+ - `logger.info(path, data)` Logs to the console just like console.log but also has a time stamp.
+
+  - `logger.pass(path, data)` Logs a message to the console with a time stamp, succsessful-tag and in the color green.
+
+  - `logger.warning(path, data)` Logs a message to the console with a time stamp, warning-tag and in the color yellow.
+
+  - `logger.error(path, data)` Logs a message to the console with a time stamp, error-tag and in the color red
+  
+  - `logger.clean(path)` Clears the log file so that there is no content from the precious log.  
+
+<br>
+<h1 align="center">Example</h1>
+
+```js
+// Import the logger 
+const logger = require("./logger");
+
+ 
+//Define the log path 
+const path = './log.txt';
+
+//Removes all content from the log file
+logger.clean(path)
+
+
+
+//Logs  --> "[16:49:7] Default Test" in grey
+logger.info(path, 'Default Test')
+
+
+
+//logs -->  "[16:49:7][successful] Pass Test" in green
+logger.pass(path, 'Pass Test')
+
+
+
+//logs --> " [16:49:7][Warning] Warning Test" in yellow
+logger.warning(path,'Warning Test ')
+
+
+
+//logs --> " [16:49:7][Error] Error Test" in red
+logger.error(path,'Error Test')
+```
+
+<hr>
+<p>Made with ❤️ by: mini51</p>
---
+++ logger.js
@@ -0,0 +1,78 @@
+const fs = require('fs')
+
+
+//* define style for each type of log
+const colors = {
+
+    Pass: "\x1b[32m", 
+    Warn: "\x1b[33m", 
+    Error: "\x1b[31m",
+    Reset: "\x1b[0m"
+
+}
+
+//* Get current time and then seperates it into hrs min sec
+const day = new Date()
+const hrs = day.getHours();
+const min = day.getMinutes();
+const sec = day.getSeconds();
+
+
+//* Setting up var
+var formated;
+
+
+module.exports = {
+    
+
+
+    //* when called will log normal messages to the console and debuf file    
+    info: function(path, data) {
+
+        formated = `[${hrs}:${min}:${sec}] ${data}`
+
+        console.log(formated)
+        fs.appendFile(path,formated + '\n', () => { 
+
+        })
+    },
+
+    //*  When called will log pass messages to console and debug file  
+    pass: function(path, data) {
+
+        formated = `[${hrs}:${min}:${sec}][successful] ${data}`
+
+        console.log(colors.Pass,formated, colors.Reset)
+        fs.appendFile(path,formated + '\n', () => { 
+
+        })
+    },
+    
+    //* When called will log warnings to the consoole and debug file
+    warning: function(path, data) {
+        formated = `[${hrs}:${min}:${sec}][Warning] ${data}`
+
+        console.log(colors.Warn,formated, colors.Reset)
+        fs.appendFile(path,formated + '\n', () => { 
+
+        })
+    },
+
+    //* When called will log errors to the consoole and debug file
+    error: function(path, data) {
+        formated = `[${hrs}:${min}:${sec}][Error] ${data}`
+
+        console.log(colors.Error,formated, colors.Reset)
+        fs.appendFile(path,formated + '\n', () => { 
+
+        })
+
+    }, 
+    
+    //* Clears log file 
+    clean: function(path) {
+        fs.truncate(path, 0 , () => { 
+
+        })
+    }
+};
